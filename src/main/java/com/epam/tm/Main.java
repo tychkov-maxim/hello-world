2e81bc0ad44e67ddfaf8936cd5eeb9a750d0bec4
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-09-28 22:26:09 +0200

    add String

--- src/main/java/com/epam/tm/Main.java
+++ src/main/java/com/epam/tm/Main.java
@@ -2,6 +2,7 @@ package com.epam.tm;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld " +
+                "New Commit");
     }
 }
